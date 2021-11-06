01eb26b79cb6fda6fc1a184b1c40305aa68657dd
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-06 14:57:15 -0600

    corrected file path for js in html file (was showing previous local path), added alert to display stats for the games and included logic to add to a total games variable which displays during the stats alert

--- rock-paper-scissors/assets/js/script.js
+++ rock-paper-scissors/assets/js/script.js
@@ -2,6 +2,7 @@
 var wins = 0;
 var losses = 0;
 var ties = 0;
+var totalGames = 0;
 
 
 //Functions
@@ -11,7 +12,6 @@ function startGame() {
 //Sets userChoice logic
 
     var userChoice = prompt("Please choose Rock, Paper or Scissors");
-
     console.log(userChoice);
 
 //Sets computerChoice logic
@@ -19,31 +19,45 @@ function startGame() {
     var computerOptions = ["Rock", "Paper", "Scissors"]
 
     var randomNum = Math.floor(Math.random() * computerOptions.length);
+    console.log(randomNum);
 
     var computerChoice = computerOptions[randomNum];
+    console.log(computerChoice);
     alert("Computer chose " + computerChoice + "!");
 
 //If statement for determining winner
-    if (userChoice === "Rock" && computerChoice !== "Paper") {
+    if (userChoice === "Rock" && computerChoice === "Scissors") {
         alert("You've won!");
         wins++;
-    } else if  (userChoice === "Paper" && computerChoice !== "Scissors") {
+        totalGames++;
+    } else if  (userChoice === "Paper" && computerChoice === "Rock") {
         alert("You've won!");
         wins++;
-    }  else if (userChoice === "Scissors" && computerChoice !== "Rock") {
+        totalGames++;
+    }  else if (userChoice === "Scissors" && computerChoice === "Paper") {
         alert("You've won!");
         wins++;
+        totalGames++;
     } else if (userChoice === computerChoice) {
         alert("This was a tie!");
         ties++;
-        console.log(ties);
+        totalGames++;
         } else {
         alert("Computer wins!")
         losses++;
+        totalGames++;
     };
+
+//Current stats are displayed to the user
+    alert("Stats so Far\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties + "\nTotal Games Played: " + totalGames);
 };
 
 startGame();
+
+
+//User confirms whether they want to play again
+// var playAgain = confirm("Do you want to play again?")
+
 
 
 
