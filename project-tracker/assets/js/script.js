5571284887613498e7021f4821a64008204cac5a
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-22 19:50:32 -0700

    Added variables for DOM elements that will be manipulated in js, started on event listener for form submit

--- project-tracker/assets/js/script.js
+++ project-tracker/assets/js/script.js
@@ -1,15 +1,44 @@
 //VARIABLE DECLARATIONS
+var projectFormEl = $("#project-form");
+var projectNameEl = $("#project-name");
+var projectTypeEl = $("#project-type");
+var projectWageEl = $("#project-wage");
+var projectDueEl = $("#project-due-date");
 var dateTime = $("#dateTime");
 
 $(dateTime).text(moment().format("LLLL"));
 
 //FUNCTION DECLARATIONS
+//Retrieves current date-time
 function updateTime() {
   dateTime.text(moment().format("LLLL"));
 }
-
+//Updates date-time every 30 seconds
 setInterval(function () {
   updateTime();
 }, 30000);
 
 //EVENT LISTENERS
+projectFormEl.submit(function (event) {
+  var nameInput = projectNameEl.val();
+  event.preventDefault();
+  console.log(nameInput);
+});
+
+//Psuedo Coding
+/*When user presses the submit button, the fields
+of the form are printed to the table.
+ 
+
+1st f - capture input from the 4 fields on the form
+2nd f - prints input from 1st f to 2nd, 
+accepts 4 args, 1 for each input from 1st f 
+ Dynamically create tr, save to variable
+ Dynamically create tds
+ For days to due date, use Moment to calc difference
+ between current date and due date
+ For est amount, hr rate * 8, then * days to due date
+ Append created tds to tr
+ Append tr to tbody
+ Close modal at end
+*/
