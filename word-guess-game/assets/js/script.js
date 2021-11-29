7828518a2f7e2546af73fced01f136e88786002c
Caitlin Roberts <caitlin.coy.roberts@gmail.com>
2021-11-28 17:14:50 -0700

    added setInterval timer to game, included win/fail comparisons

--- word-guess-game/assets/js/script.js
+++ word-guess-game/assets/js/script.js
@@ -1,11 +1,18 @@
 //VARIABLE DECLARATIONS
 
-//store references to dom elements in variables
+//DOM Element Selections
+var wordGuess = document.querySelector(".word-blanks");
+var startButton = document.querySelector(".start-button");
+var timerCount = document.querySelector(".timer-count");
+var winEl = document.querySelector(".win");
+var loseEl = document.querySelector(".lose");
+
+//Counters
 var wins = 0;
 var losses = 0;
+var sec = 20;
 
-var count = 20;
-
+//Word Selection
 var words = [
   "Han",
   "Chewbacca",
@@ -19,11 +26,6 @@ var words = [
   "Iden",
   "Jyn",
 ];
-
-var wordGuess = document.querySelector(".word-blanks");
-
-var startButton = document.querySelector(".start-button");
-
 var randomWord = "";
 var displayWord = "";
 
@@ -42,38 +44,65 @@ function getRandomWord() {
   wordGuess.textContent = displayWord;
 }
 
-startButton.addEventListener("click", getRandomWord);
+//Listens for user to press Start button and if so, retrieves a random word from the words array
+startButton.addEventListener("click", startGame);
 
-//will need to capture keys pressed
-//compare whether it is part of the word that has been chosen and display the keys in the word that match
-
+//Listens for key press from user and if so, runs function to compare randomWord variable with keys pressed
 document.addEventListener("keydown", keyPressed);
 
 function keyPressed(event) {
+  //Checks key presses from user to compare to letters in random word and replaces underscores with that letter if it matches
   var wordCheck = randomWord.toLowerCase();
   var displayWordArray = displayWord.split("");
-  console.log(displayWordArray);
   console.log(wordCheck);
   if (wordCheck.includes(event.key)) {
     for (i = 0; i < wordCheck.length; i++) {
       if (event.key === wordCheck[i]) {
         displayWordArray[i] = event.key;
         displayWord = displayWordArray.join("");
+        console.log(displayWordArray);
+        console.log(displayWord);
         wordGuess.textContent = displayWord;
       }
     }
   }
+
+  checkAnswer();
 }
 
-//need to create a timer: setInterval, clearInterval
-setInterval(gameOver, 3000);
+//Starts countdown timer
+var timerFunction = setInterval(startTimer, 1000);
 
-function gameOver() {
-  if (displayWord != randomWord) {
-    wordGuess.textContent = "GAME OVER";
+function startTimer() {
+  sec--;
+  timerCount.textContent = sec;
+  if (sec < 1) {
+    gameOver();
   }
 }
 
-//display "GAME OVER" in same spot as the blank word once time is up - last piece I work on
+function gameOver() {
+  clearInterval(timerFunction);
+  if (displayWord != randomWord.toLowerCase()) {
+    wordGuess.textContent = "GAME OVER";
+    losses++;
+    loseEl.textContent = losses;
+  }
+}
+
+function checkAnswer() {
+  if (displayWord === randomWord.toLowerCase()) {
+    clearInterval(timerFunction);
+    wordGuess.textContent = "Success!";
+    wins++;
+    winEl.textContent = wins;
+  }
+}
+
+function startGame() {
+  getRandomWord();
+}
+
+//save wins and losses to local storage
 
 //display success message if user gets all the letters
